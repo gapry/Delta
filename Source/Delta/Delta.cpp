e4f6bc4123c0cdfeee0111011bec39f2746eea70
gapry <gapry@protonmail.com>
2021-04-29 11:18:26 +0800

    Delta: init

---
+++ Source/Delta/Delta.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Delta.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Delta, "Delta");
