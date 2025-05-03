76692cbcb7d8ebc5e74df4548017d8f7babb8356
gapry <gapry@protonmail.com>
2025-05-04 05:21:06 +0800

    vcpkg.bat: fix

--- GenerateVSCodeWorkspace.bat
+++ GenerateVSCodeWorkspace.bat
@@ -1,3 +1,7 @@
+REM Copyright (c) 2025 Gapry.
+REM Licensed under the MIT License.
+REM See LICENSE file in the project root for full license information.
+
 @echo off
 
 set UE_PATH=D:\Epic Games\UE_5.5
--- vcpkg.bat
+++ vcpkg.bat
@@ -1,6 +1,13 @@
+REM Copyright (c) 2025 Gapry.
+REM Licensed under the MIT License.
+REM See LICENSE file in the project root for full license information.
+
 @echo off
 
-git clone https://github.com/microsoft/vcpkg.git
-cd vcpkg
-bootstrap-vcpkg.bat
-./vcpkg.exe install fmt
+if not exist vcpkg (
+  git clone https://github.com/microsoft/vcpkg.git
+)
+
+call .\vcpkg\bootstrap-vcpkg.bat
+
+call .\vcpkg\vcpkg.exe install fmt
