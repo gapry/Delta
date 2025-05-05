3f467202a8bb9e883e0a82aa833d0cbc25a5c369
gapry <gapry@protonmail.com>
2025-05-06 06:46:55 +0800

    DebugShape: init

--- Source/Delta/Item/Item.cpp
+++ Source/Delta/Item/Item.cpp
@@ -4,6 +4,7 @@
 
 #include "Item.h"
 #include "../Common/LogUtil.h"
+#include "../Common/DebugShape.h"
 
 AItem::AItem() {
   PrimaryActorTick.bCanEverTick = true;
@@ -13,6 +14,14 @@ void AItem::BeginPlay() {
   Super::BeginPlay();
 
   DELTA_LOG("{}", DELTA_FUNCSIG);
+
+  const FVector CurrentLocation = GetActorLocation();
+  const FVector CurrentForward  = GetActorForwardVector();
+
+  DELTA_DEBUG_POINT(CurrentLocation);
+  DELTA_DEBUG_LINE(CurrentLocation, CurrentLocation + CurrentForward * 100.f);
+  DELTA_DEBUG_ARROW(CurrentLocation, CurrentLocation + CurrentForward * 150.f);
+  DELTA_DEBUG_SPHERE(CurrentLocation);
 }
 
 void AItem::Tick(float DeltaTime) {
