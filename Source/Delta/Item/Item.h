15864ca1a893cceaa8e5fbdaa5dca3bc97d256aa
gapry <gapry@protonmail.com>
2025-05-06 19:46:45 +0800

    Item: add SetRotation (#14)

--- Source/Delta/Item/Item.h
+++ Source/Delta/Item/Item.h
@@ -17,14 +17,21 @@ public:
   virtual void Tick(float DeltaTime) override;
 
   void SetLocation(const FVector& NewLocation);
+  void SetRotation(const FRotator& NewRotation);
+  void SetForwardDirection(const FVector& NewForwardDirection);
+
+  void RenderDebugShape() const;
 
 protected:
   virtual void BeginPlay() override;
 
 private:
   UPROPERTY(EditAnywhere, Category = "Item")
-  FVector CurrentLocation;
+  FVector CurrentLocation = FVector::ZeroVector;
 
   UPROPERTY(EditAnywhere, Category = "Item")
-  FVector ForwardDirection;
+  FVector ForwardDirection = FVector::ForwardVector;
+
+  UPROPERTY(EditAnywhere, Category = "Item")
+  FRotator CurrentRotation = FRotator::ZeroRotator;
 };
