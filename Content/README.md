14fe8d34b5126c7b02fdd242233b153db6c52117
gapry <gapry@protonmail.com>
2025-05-09 04:27:47 +0800

    Pawn: init

--- Content/README.md
+++ Content/README.md
@@ -1,2 +1,5 @@
 ## Unreal Engine
 - Starter Content
+
+## Fab
+- [ANIMAL VARIETY PACK](https://www.fab.com/listings/2dd7964c-a601-4264-a53d-465dcae1644c)
---
+++ Source/Delta/Pawn/Bird.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2025 Gapry.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
+#include "Bird.h"
+#include "Components/SkeletalMeshComponent.h"
+
+ABird::ABird() {
+  PrimaryActorTick.bCanEverTick = true;
+
+  static const TCHAR* const ComponentName = TEXT("SkeletalMeshComponent");
+  SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(ComponentName);
+
+  if (SkeletalMeshComponent) {
+    RootComponent = SkeletalMeshComponent;
+  }
+
+  static const TCHAR* const SkeletalMeshPath =
+    TEXT("SkeletalMesh'/Game/AnimalVarietyPack/Crow/Meshes/SK_Crow.SK_Crow'");
+  InitializeSkeletalMeshComponent(SkeletalMeshPath);
+}
+
+void ABird::BeginPlay() {
+  Super::BeginPlay();
+}
+
+void ABird::Tick(float DeltaTime) {
+  Super::Tick(DeltaTime);
+}
+
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ABird::InitializeSkeletalMeshComponent(const TCHAR* const SkeletalMeshPath) {
+  if (SkeletalMeshComponent) {
+    SkeletalMeshComponent->SetMobility(EComponentMobility::Movable);
+    if (SkeletalMeshPath != nullptr) {
+      static ConstructorHelpers::FObjectFinder<USkeletalMesh> Finder(SkeletalMeshPath);
+      if (Finder.Succeeded()) {
+        SkeletalMeshComponent->SetSkeletalMesh(Finder.Object);
+      }
+    }
+  }
+}
