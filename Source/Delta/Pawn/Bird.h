c136c1ed8469a9b949ffd7e6f2e6722db0f4e929
gapry <gapry@protonmail.com>
2025-05-09 04:56:11 +0800

    Pawn: init (#19)

---
+++ Source/Delta/Pawn/Bird.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2025 Gapry.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+class UInputComponent;
+class USkeletalMeshComponent;
+
+UCLASS()
+class DELTA_API ABird : public APawn {
+  GENERATED_BODY()
+
+public:
+  ABird();
+
+  virtual void Tick(float DeltaTime) override;
+
+  virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+  void InitializeSkeletalMeshComponent(const TCHAR* const SkeletalMeshPath);
+  void PostInitializeSkeletalMeshComponent();
+
+protected:
+  virtual void BeginPlay() override;
+  virtual void PostInitializeComponents() override;
+
+private:
+  UPROPERTY(VisibleAnywhere,
+            BlueprintReadOnly,
+            Category = "Bird",
+            meta     = (AllowPrivateAccess = "true"))
+  TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent{nullptr};
+};
