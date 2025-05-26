494351e56f3a77fc3ecfca0a3c565dde836b5c67
gapry <gapry@protonmail.com>
2025-05-26 19:22:45 +0800

    character: add animation blueprint

---
+++ Source/Delta/Character/EchoAnimInstance.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2025 Gapry.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
+#include "EchoAnimInstance.h"
+#include "EchoCharacter.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UEchoAnimInstance::NativeInitializeAnimation() {
+  Super::NativeInitializeAnimation();
+
+  EchoCharacter = Cast<AEchoCharacter>(TryGetPawnOwner());
+  if (EchoCharacter) {
+    CharacterMovement = EchoCharacter->GetCharacterMovement();
+  }
+}
+
+void UEchoAnimInstance::NativeUpdateAnimation(float DeltaTime) {
+  Super::NativeUpdateAnimation(DeltaTime);
+
+  if (CharacterMovement) {
+    GroundSpeed = UKismetMathLibrary::VSizeXY(CharacterMovement->Velocity);
+  }
+}
---
+++ Source/Delta/Character/EchoAnimInstance.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2025 Gapry.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EchoAnimInstance.generated.h"
+
+class AEchoCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class DELTA_API UEchoAnimInstance : public UAnimInstance {
+  GENERATED_BODY()
+
+public:
+  virtual void NativeInitializeAnimation() override;
+  virtual void NativeUpdateAnimation(const float DeltaTime) override;
+
+private:
+  UPROPERTY(BlueprintReadOnly, Category = "EchoAnimInstance", meta = (AllowPrivateAccess = "true"))
+  TSoftObjectPtr<AEchoCharacter> EchoCharacter;
+
+  UPROPERTY(BlueprintReadOnly, Category = "EchoAnimInstance", meta = (AllowPrivateAccess = "true"))
+  TSoftObjectPtr<UCharacterMovementComponent> CharacterMovement;
+
+  UPROPERTY(BlueprintReadOnly, Category = "EchoAnimInstance", meta = (AllowPrivateAccess = "true"))
+  float GroundSpeed{0.0f};
+};
