8e63e5ccd421ceb7c4896c98e125fcaba0c5db7c
gapry <gapry@protonmail.com>
2025-06-01 17:16:10 +0800

    breakable: set the collision in the base class

--- Source/Delta/Breakable/Pot_A_Complete.cpp
+++ Source/Delta/Breakable/Pot_A_Complete.cpp
@@ -7,22 +7,7 @@
 #include "../Common/Finder.h"
 
 APot_A_Complete::APot_A_Complete() {
-  {
-    static const TCHAR* const Path = TEXT("/Script/GeometryCollectionEngine.GeometryCollection'/Game/Delta/Destructible/"
-                                          "GC_SM_Pot_A_Complete1.GC_SM_Pot_A_Complete1'");
-    DELTA_SET_GEOMETRY_COLLECTION(GeometryCollectionComponent, Path);
-  }
-
-  {
-    GeometryCollectionComponent->bUseSizeSpecificDamageThreshold = false;
-    GeometryCollectionComponent->SetGenerateOverlapEvents(true);
-    GeometryCollectionComponent->SetNotifyRigidBodyCollision(false);
-
-    GeometryCollectionComponent->SetCollisionProfileName(TEXT("Custom"));
-    GeometryCollectionComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-    GeometryCollectionComponent->SetCollisionObjectType(ECollisionChannel::ECC_Destructible);
-    GeometryCollectionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
-    GeometryCollectionComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldDynamic, ECollisionResponse::ECR_Overlap);
-    GeometryCollectionComponent->UpdateCollisionProfile();
-  }
+  static const TCHAR* const Path = TEXT("/Script/GeometryCollectionEngine.GeometryCollection'/Game/Delta/Destructible/"
+                                        "GC_SM_Pot_A_Complete1.GC_SM_Pot_A_Complete1'");
+  DELTA_SET_GEOMETRY_COLLECTION(GeometryCollectionComponent, Path);
 }
