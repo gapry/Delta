3d3109218bae8876cf0564af135a8168efcdd454
gapry <gapry@protonmail.com>
2025-06-06 23:46:49 +0800

    enemy:health: add method to set the percent: fix

--- Source/Delta/Component/HealthBarComponent.cpp
+++ Source/Delta/Component/HealthBarComponent.cpp
@@ -5,13 +5,28 @@
 #include "HealthBarComponent.h"
 #include "Components/ProgressBar.h"
 #include "../HUD/HealthBar.h"
+#include "../Common/LogUtil.h"
 
 void UHealthBarComponent::SetHealthPercent(const float Percent) {
-  if (HealthBarWidget == nullptr) {
-    HealthBarWidget = Cast<UHealthBar>(GetUserWidgetObject());
+  if (Percent < 0.0f || Percent > 1.0f) {
+    DELTA_LOG("{}", DeltaFormat("Health percent must be between 0.0 and 1.0, received: {}", Percent));
+    return;
   }
 
-  if (HealthBarWidget && HealthBarWidget->HealthBar) {
-    HealthBarWidget->HealthBar->SetPercent(Percent);
+  UUserWidget* CurrentWidget = GetUserWidgetObject();
+  if (CurrentWidget == nullptr) {
+    DELTA_LOG("{}", DeltaFormat("HealthBarComponent widget is not initialized or not set."));
+    return;
   }
+
+  if (HealthBarWidget == nullptr) {
+    HealthBarWidget = Cast<UHealthBar>(CurrentWidget);
+  }
+
+  if (HealthBarWidget->HealthBar == nullptr) {
+    DELTA_LOG("{}", DeltaFormat("HealthBarWidget's HealthBar is not initialized or not set."));
+    return;
+  }
+
+  HealthBarWidget->HealthBar->SetPercent(Percent);
 }
